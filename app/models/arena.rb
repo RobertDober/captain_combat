619cb2bf606a3ff3fdeba0be0aa0168deb7bb7b3
RobertDober <robert.dober@gmail.com>
2019-09-14 18:13:58 +0200

    Failing tests pushed on purpose to check Travis integration with PRs

--- app/models/arena.rb
+++ app/models/arena.rb
@@ -1,2 +1,4 @@
 class Arena < ApplicationRecord
+
+  validates :name, uniqueness: true
 end
--- spec/models/arena_spec.rb
+++ spec/models/arena_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Arena, type: :model do
+
+  describe "validation of uniqueness" do
+
+    it "is valid if the names differ" do
+      create :arena
+      arena1 = build :arena
+      expect(arena1).to be_valid
+    end
+
+    it "is not valid if they are the same" do
+      arena = create :arena
+      arena1 = build(:arena, arena_name: arena.arena_name)
+      expect( arena1 ).not_to be_valid
+    end
+  end
 end
